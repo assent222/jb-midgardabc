04616f44f9dc91f0d11376380bbd7b16e58eb647
oyushche <uhmateush@gmail.com>
2013-12-10 18:22:26 +0200

    home work 1

---
+++ jb-dec-oleg.yushchenko/src/com/midgardabc/day1/hw1/BattleField.java
@@ -0,0 +1,16 @@
+package com.midgardabc.day1.hw1;
+
+public class BattleField {
+
+	public static void main(String[] args) {
+		System.out.println("B B B B B B B B B");
+		System.out.println("C               C");
+		System.out.println("B B B   R   B B B");
+		System.out.println("B B B       B B B");
+		System.out.println("B B     R     B B");
+		System.out.println("B B W R R R W B B");
+		System.out.println("B B W W W W W B B");
+		System.out.println("B     B B B     B");
+		System.out.println("B   T B E B     B");
+	}
+}
